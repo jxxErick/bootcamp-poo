666dff6dd5c5689fdb271a8ecf90dc37334c1c18
jxxErick <ericksdantas144@gmail.com>
2023-05-04 01:39:43 -0300

    finished

--- src/br/com/bootcamp/domain/Curso.java
+++ src/br/com/bootcamp/domain/Curso.java
@@ -1,32 +1,17 @@
 package br.com.bootcamp.domain;
 
-public class Curso {
+public class Curso extends Conteudo{
 
-   private String titulo;
-    private String descricao;
     private int cargaHoraria;
 
-    public Curso(String titulo, String descricao, int cargaHoraria) {
-        this.titulo = titulo;
-        this.descricao = descricao;
-        this.cargaHoraria = cargaHoraria;
+    @Override
+    public double calcularXp() {
+        return XP_PADRAO * cargaHoraria;
     }
 
-    public String getTitulo() {
-        return titulo;
+    public Curso() {
     }
 
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
 
     public int getCargaHoraria() {
         return cargaHoraria;
@@ -39,8 +24,8 @@ public class Curso {
     @Override
     public String toString() {
         return "Curso{" +
-                "titulo='" + titulo + '\'' +
-                ", descricao='" + descricao + '\'' +
+                "titulo='" + getTitulo() + '\'' +
+                ", descricao='" + getDescricao() + '\'' +
                 ", cargaHoraria=" + cargaHoraria +
                 '}';
     }
